07dfe188e26a65b0b9d1e73498c21cc43b825a96
mohammad almasri <127051988+Moh097@users.noreply.github.com>
2024-07-29 08:50:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from huggingface_hub import login
 def main():
     counter = 1
 
-    pdf_path = "C:/Users/moham/Downloads/questioning-the-influence-of-political-trust-on-individuals-unconventional-political-participation-in-palestine.pdf"
-    output_docx_path = "C:/Users/moham/OneDrive/Desktop/Translator/translated_document.docx"
+    pdf_path = "path/to/your/document.pdf"
+    output_docx_path = "path/to/save/translated_document.docx"
 
     try:
         pdf_reader = PdfReader(pdf_path)
